97b90bc4a19ccf62ad4f36b1632569dcdb198e00
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-08 11:53:44 -0800

    trying to make enviromnet variables

--- tests.py
+++ tests.py
@@ -1,22 +1,13 @@
-from weather import Weather
-weather = Weather()
+from imgurpython import ImgurClient
+import os
+
+client_id = '71f3976ee48114b'
+client_secret = os.environ.get('secret')
+
+client = ImgurClient(client_id, client_secret)
 
 def main(): 
-
-	x = 'henderson'
-
-	location = weather.lookup_by_location(x)
-	condition = location.condition()
-	print(condition.text())
-
-	#
-	#forecasts = location.forecast()
-	#for forecast in forecasts:
-	#    print(forecast.text())
-	#    print(forecast.date())
-	#    print(forecast.high())
-	#    print(forecast.low())
-
+	print('test 1 2 ')
 
 if __name__ == '__main__': 
 	main()
