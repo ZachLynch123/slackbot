7e8d172dafed0f30690bb52ddb2da35e47aa6497
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-08 07:07:50 -0800

    Added a ditto command and attemting to intigrate the weather api

--- starterbot.py
+++ starterbot.py
@@ -36,7 +36,21 @@ def handle_command(command,channel):
 	# check if the response starts with the example command
 	if command.startswith(EXAMPLE_COMMAND): 
 		response = "Well.. I can't really do anything right now, but I'll be able to do some cool stuff soon!"
+	if command.startswith("ditto"): 
+		message = command[6:]
+		response = message
+	if command.startswith("forecast"): 
+		location = command[8:]
+		forecasts = location.forecast()
+		for forecast in forceasts: 
+		print(forecast.text())
+	    print(forecast.date())
+	    print(forecast.high())
+	    print(forecast.low())
+
+
 	slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+
 
 
 
