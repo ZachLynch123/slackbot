512460af46d8e6d81c39dfe39967dec8cc253824
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-09 12:17:30 -0800

    tests.py takes all the picture urls from the front page of imgur and sets it to a list. Printing one randomly when function is called

--- tests.py
+++ tests.py
@@ -1,20 +1,23 @@
 from imgurpython import ImgurClient
 import os
+import random
 
+# authentification for imgur  using environmental variables
 client_id = os.environ.get('IMGUR_ID')
 client_secret = os.environ.get('IMGUR_SECRET')
-print(client_secret)
-
+# set client object
 client = ImgurClient(client_id, client_secret)
 
+# function that takes all the pictures from the front page of imgur and adds to a list
 def main(): 
 	url_list = []
 	items = client.gallery()
 	i = 0
 	for item in items:
 		url_list.append(item.link) 
-		print(url_list[i])
-		i = i + 1
+			i = i + 1
+	# takes a random url from the list and prints it out on the screen
+	print(random.choice(url_list))
 
 if __name__ == '__main__': 
 	main()
