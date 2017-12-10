a3efcf013d3732a9f682fe9c6a83145d7e4826a0
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-10 07:33:44 -0800

    Edited the bot's first class. Letting it connect to the slack channel

--- bot.py
+++ bot.py
@@ -1,1 +1,26 @@
-bot.py
+import time
+import event
+import os
+from slackclient import SlackClient
+
+class Bot(object): 
+	def __init__(self): 
+		self.slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+		self.bot_name = "ankha"
+		self.bot_id = os.environ.get('BOT_ID')
+
+	if self.bot_id = None: 
+		exit("Error! Could not find " + self.bot_name)
+
+	self.event = event.Event(self)
+	self.listen()
+
+	def listen(self): 
+		if self.slack_client.rtm_connect(with_team_state=False): 
+			print('CRUSADER ONLINE')
+			while True: 
+				self.event.wait_for_event()
+
+				time.sleep(1)
+		else: 
+			exit("ERROR! Connection failed")
