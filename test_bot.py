eb09245d20848c6d5a3ef0315eac8dd1a8546739
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-10 10:08:35 -0800

    Hard coded token into SlackBot parameter

--- test_bot.py
+++ test_bot.py
@@ -6,6 +6,7 @@ import os
 
 class Bot(SlackBot): 
 	def __init__(self,*args, **kwargs): 
+		token = os.environ.get('SLACK_BOT_TOKEN')
 		super().__init__(*args, **kwargs)
 		self.logger = logging.getLogger(__name__)
 
