d5610781f5419921902e20d43ff8e18c23ecb3a9
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-13 17:46:28 -0800

    Showed another join, and included a reason why and when join should be used

--- tests.py
+++ tests.py
@@ -5,9 +5,13 @@ def main():
 	# Practicing string concatination
 	names = ['Jeff', 'Gary', 'Jill', 'Sam']
 
-	for name in names: 
+	#for name in names: 
 		# print('hello there, ' + name)
-		print(' '.join(['Hello there', name]))
+	#	print(' '.join(['Hello there', name]))
+	# if you don't know how join works, it's not readable
+	# but it's more efficient, if you're going to concatinate 2 or more strings, use join. It will scale better and use less processing
+	
+	print(', '.join(names))
 
 if __name__ == "__main__": 
 	main()
