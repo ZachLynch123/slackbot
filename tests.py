44513b1030645eb154fe0d5e2a9b9b837f6b4e59
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-13 17:42:49 -0800

    two different ways to print a list of names to the user. One that's more readable, and another that's more.. professional

--- tests.py
+++ tests.py
@@ -3,5 +3,11 @@ import os
 
 def main(): 
 	# Practicing string concatination
+	names = ['Jeff', 'Gary', 'Jill', 'Sam']
+
+	for name in names: 
+		# print('hello there, ' + name)
+		print(' '.join(['Hello there', name]))
+
 if __name__ == "__main__": 
 	main()
