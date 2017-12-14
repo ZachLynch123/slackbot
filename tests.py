c8e5d5b1fbab90f5c2eb0353e17226dcb67945c0
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-13 17:52:22 -0800

    Used another example as to why join is a better idea for concatination. Best for concatinating file locations and file names

--- tests.py
+++ tests.py
@@ -4,13 +4,18 @@ import os
 def main(): 
 	# Practicing string concatination
 	names = ['Jeff', 'Gary', 'Jill', 'Sam']
+	location_of_files = '/Users/Zach/myenv/project_folder/slack_bot'
+	file_name = 'requirements.txt'
+	# not the right way to go about this
+	print(location_of_files + '/' + file_name)
+	with open(os.path.join(location_of_files, file_name)) as f:
+		print (f.read())
 
 	#for name in names: 
 		# print('hello there, ' + name)
 	#	print(' '.join(['Hello there', name]))
 	# if you don't know how join works, it's not readable
 	# but it's more efficient, if you're going to concatinate 2 or more strings, use join. It will scale better and use less processing
-	
 	print(', '.join(names))
 
 if __name__ == "__main__": 
