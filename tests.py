2e5e495ccf537faec44859e2a590874c028512ac
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-13 17:39:07 -0800

    Tests.py will be practice for string concatination

--- tests.py
+++ tests.py
@@ -1,15 +1,7 @@
 from imgurpython import ImgurClient
 import os
 
-client_id = os.environ.get('IMGUR_ID')
-client_secret = os.environ.get('IMGUR_SECRET')
-client  = ImgurClient(client_id, client_secret)
-
 def main(): 
-	q = 'birds'
-	items = client.gallery_search(q, advanced=None, sort='time', window='all', page=0)
-	for item in items: 
-		print (items)
-
+	# Practicing string concatination
 if __name__ == "__main__": 
 	main()
